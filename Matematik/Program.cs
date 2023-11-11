7efb25c59567a0d0b12a2e8d275f40eadbb110b6
dianakaynak <dianaonder90@gmail.com>
2023-11-11 16:13:10 +0300

    Add project files.

---
+++ Matematik/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Matematik
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 6);
+            dortIslem.Topla(25, 6);
+
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    public class SepetManager
+    {
+        //naming convertion - isimlendirme
+        //syntax - yazım şekilleri
+        public void Ekle(Urun urun)
+        {
+
+
+            Console.WriteLine("Tebrikler,Sepete eklendi!"+ urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat,int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler sepete eklendi" + urunAdi);
+        }
+
+        
+    }
+}
