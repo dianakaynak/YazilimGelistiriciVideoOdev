dbca7f3d647daeb4aa57ce858208a1663209b6d7
dianakaynak <dianaonder90@gmail.com>
2023-11-12 11:46:24 +0300

    KoleksiyonVeGenerics

---
+++ Genericsintro/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Genericsintro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+        }
+    }
+}
